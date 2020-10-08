60802c1c0d01ef6a266d0e22dd77383402607ae7
Paul Coroneos <paul@pcoroneos.com>
2020-10-07 21:50:32 -0500

    wip

---
+++ data-structure/set-difference.js
@@ -0,0 +1,91 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // This is our union method from that lesson
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(value => {
+      newSet.add(value);
+    })
+    set.values().forEach(value => {
+      newSet.add(value);
+    })
+
+    return newSet;
+  }
+  // This is our intersection method from that lesson
+  intersection(set) {
+    const newSet = new Set();
+
+    let largeSet;
+    let smallSet;
+    if (this.dictionary.length > set.length) {
+      largeSet = this;
+      smallSet = set;
+    } else {
+      largeSet = set;
+      smallSet = this;
+    }
+
+    smallSet.values().forEach(value => {
+      if (largeSet.dictionary[value]) {
+        newSet.add(value);
+      }
+    })
+
+    return newSet;
+  }
+  // change code below this line
+  difference(setb) {
+    const diffSet = new Set();
+    this.values().forEach(val => !setb.values().includes(val) && diffSet.add(val))
+
+    return diffSet;
+  }
+  // change code above this line
+}
+
+const set = new Set();
+const setb = new Set();
+
+set.add('a');
+set.add('b');
+setb.add('a');
+setb.add('c');
+
+console.log(set.difference(setb));
---
+++ data-structure/set-intersection.js
@@ -0,0 +1,72 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // This is our union method from that lesson
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(value => {
+      newSet.add(value);
+    })
+    set.values().forEach(value => {
+      newSet.add(value);
+    })
+
+    return newSet;
+  }
+  // change code below this line
+  intersection(set) {
+    const intersect = new Set();
+    set.values().forEach(val => {
+      if(this.has(val)) {
+        intersect.add(val);
+      }
+    });
+    return intersect;
+  }
+  // change code above this line
+}
+
+const set = new Set();
+const setb = new Set();
+
+set.add('a');
+set.add('b');
+setb.add('a');
+
+console.log(set.intersection(setb))
+
